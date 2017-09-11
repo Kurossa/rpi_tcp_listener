05cca21178713c282ef489015aac8acafeea5ee5
Kurossa <mariusz.krzysiek@gmail.com>
2017-09-11 23:30:18 +0200

    Update sdk cmake and documentation

--- README.md
+++ README.md
@@ -1,13 +1,36 @@
-# rpi_tcp_listener
+# RaspberryPi tcp server
+This application implements tcp server. It implements simple plain text protocol to control mp3 player using RapsberryPi. It supports configuration from file, reconfiguration of network setings like: ip, mask or gateway.
 
-# Compile
-## Native compilation
+# Requirements
+This is linux based application. Requires pthread, mpg123 and ao external libs. It was compiled and tested on x86 linux and RaspberryPi 1,2 and 3.
+
+# Builting target
+## Native build
 `$ cmake -DCMAKE_BUILD_TYPE=<type> <path_to_project>`
 `$ make`
 type: Debug, Release
 path_to_project: location of rpi_tcp_listener sources
 
-## Cross-compilation
-`$ cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_TOOLCHAIN_FILE=$HOME/rpi_original/toolchain_raspberry_pi.cmake ../
-`
+## Cross-compilation build
+### Prepare RaspberryPi sdk
+Get RaspberryPi toolchain
+`$ mkdir ~/rpi_sdk`
+`$ cd ~/rpi_sdk`
+`$ git clone https://github.com/raspberrypi/tools.git`
+Toolchain we will use is located here:
+`tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64/`
+Prepare rootfs based on target, this way we will have access to includes and libs from target.
+`$ cd ~/rpi_sdk`
+`$ mkdir rootfs`
+`$ make`
+`$ make`
+
+`$ make`
+`$ make`
+
+#### Build
+`$ cd <project_folder>`
+`$ mkdir build`
+`$ cd build`
+`$ cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_TOOLCHAIN_FILE=../toolchain_raspberry_pi.cmake ../`
 `$ make`
--- toolchain_raspberry_pi.cmake
+++ toolchain_raspberry_pi.cmake
@@ -1,22 +1,21 @@
-SET(CMAKE_SYSTEM_NAME Linux)
-SET(CMAKE_SYSTEM_VERSION 1)
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_VERSION 1)
+# SDK location
+set(SDK_PATH $ENV{HOME}/rpi_sdk)
 
 # Specify the cross compiler
-SET(CMAKE_C_COMPILER $ENV{HOME}/rpi_original/tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64/bin/arm-linux-gnueabihf-gcc)
-SET(CMAKE_CXX_COMPILER $ENV{HOME}/rpi_original/tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64/bin/arm-linux-gnueabihf-g++)
+set(CMAKE_C_COMPILER ${SDK_PATH}/tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64/bin/arm-linux-gnueabihf-gcc)
+set(CMAKE_CXX_COMPILER ${SDK_PATH}/tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64/bin/arm-linux-gnueabihf-g++)
 
-# Where is the target environment
-SET(CMAKE_FIND_ROOT_PATH $ENV{HOME}/rpi_original/rootfs)
-SET(CMAKE_SYSROOT "${CMAKE_FIND_ROOT_PATH}")
-#SET(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} --sysroot=${CMAKE_FIND_ROOT_PATH}" CACHE INTERNAL "" FORCE)
-#SET(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} --sysroot=${CMAKE_FIND_ROOT_PATH}" CACHE INTERNAL "" FORCE)
-#SET(CMAKE_MODULE_LINKER_FLAGS "${CMAKE_MODULE_LINKER_FLAGS} --sysroot=${CMAKE_FIND_ROOT_PATH}" CACHE INTERNAL "" FORCE)
+# Set sysroot enviroment
+set(CMAKE_FIND_ROOT_PATH ${SDK_PATH}/rootfs)
+set(CMAKE_SYSROOT "${CMAKE_FIND_ROOT_PATH}")
 
 # Search for programs only in the build host directories
-SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 
 # Search for libraries and headers only in the target directories
-SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
-SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 
 
