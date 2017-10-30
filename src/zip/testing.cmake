3efdd2e70ac37788a8eb72d0aacbaaf776443251
Kurossa <mariusz.krzysiek@gmail.com>
2017-10-30 22:43:24 +0100

    Update testing section, extract testing.cmake file for zip

---
+++ src/zip/testing.cmake
@@ -0,0 +1,12 @@
+if (TEST)
+	include_directories(${gtest_SOURCE_DIR}/include)
+	add_executable(zip_test
+		test/zip_test.cpp)
+
+	target_link_libraries(zip_test 
+		zip
+		gtest
+		gtest_main)
+
+	add_test(zip_test zip_test)
+endif()
